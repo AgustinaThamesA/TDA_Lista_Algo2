f7edd97edf99287577852c35b908fb3c4a1bc8f8
athames <athames@fi.uba.ar>
2024-04-11 19:00:46 -0300

    Hay 6 pruebas de lista, no pasa una: ✗ Inserción correcta de elementos en lista vacía.

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,87 @@
 #include "pa2m.h"
+#include "src/lista.h"
+#include <stdio.h>
 
-void prueba_simple()
-{
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+typedef struct nodo {
+	void *elemento;
+	struct nodo *siguiente;
+} nodo_t;
+
+struct lista {
+	nodo_t *nodo_inicio;
+	nodo_t *nodo_final;
+	size_t espacios;
+};
+
+struct lista_iterador {
+	nodo_t *actual;
+	lista_t *lista;
+	nodo_t *anterior;
+	int sarasa;
+};
+
+void crear_lista_devuelve_lista_con_nodos_null_y_espacios_en_0(){
+	lista_t* lista = lista_crear();
+	pa2m_afirmar(
+		lista->nodo_inicio == NULL && 
+		lista->nodo_final == NULL && 
+		lista->espacios == 0, 
+		"Se crea una lista con nodos en NULL y espacios en 0.");
+
+	lista_destruir(lista);
+}
+
+void crea_lista_vacia(){
+	lista_t* lista = lista_crear();
+	bool lista_esta_vacia = lista_vacia(NULL);
+	pa2m_afirmar(lista_esta_vacia == true, "Lista NULL es una lista vacía.");
+
+	lista_esta_vacia = lista_vacia(lista);
+	pa2m_afirmar(lista_esta_vacia == true, "Lista creada no tiene elementos (vacía).");
+
+	lista_insertar(lista, "prueba_vacia1");
+	lista_insertar(lista, "prueba_vacia2");
+	lista_esta_vacia = lista_vacia(lista);
+	pa2m_afirmar(lista_esta_vacia == false, "Lista con elementos no está vacía.");
+
+	lista_destruir(lista);
+}
+
+void inserta_elementos_en_lista(){
+	lista_t* lista = lista_crear();
+	char* prueba_1 = "prueba elemento 1";
+	char* prueba_2 = "prueba elemento 2";
+
+	lista_t* lista_con_elementos = lista_insertar(NULL, prueba_1);
+	pa2m_afirmar(lista_con_elementos == NULL, "No inserta un elemento en una lista NULL.");
+
+
+
+	lista_destruir(lista);
+
+	lista = lista_crear();
+	lista_insertar(lista, prueba_1);
+	lista_con_elementos = lista_insertar(lista, prueba_2);
+	pa2m_afirmar(lista_con_elementos == 0 && 
+		lista->nodo_inicio->elemento == prueba_1 && 
+		lista->nodo_final->elemento == prueba_2 && 
+		lista->espacios == 2, 
+		"Inserción correcta de elementos en lista vacía.");
+
+
+	lista_destruir(lista);
 }
 
 int main()
 {
 	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+		"\n======================== Pruebas creación ========================");
+	crear_lista_devuelve_lista_con_nodos_null_y_espacios_en_0();
+	crea_lista_vacia();
+
+	pa2m_nuevo_grupo(
+		"\n======================== Pruebas inserción elementos ========================");
+	inserta_elementos_en_lista();
 
 	return pa2m_mostrar_reporte();
 }
--- src/lista.c
+++ src/lista.c
@@ -9,28 +9,109 @@ typedef struct nodo {
 
 struct lista {
 	nodo_t *nodo_inicio;
-	//algo mas?
+	nodo_t *nodo_final;
+	size_t espacios;
 };
 
 struct lista_iterador {
-	//y acá?
+	nodo_t *actual;
+	lista_t *lista;
+	nodo_t *anterior;
 	int sarasa;
 };
 
 lista_t *lista_crear()
 {
-	return NULL;
+	lista_t *lista = malloc(sizeof(lista_t));
+	if (lista == NULL){
+		return NULL;
+	}
+	lista->nodo_inicio = NULL;
+	lista->nodo_final = NULL;
+	lista->espacios = 0;
+
+	return lista;
+}
+
+nodo_t* nuevo_nodo(void* elemento){
+	nodo_t* nodo = malloc(sizeof(nodo_t));
+	if(nodo == NULL)
+		return NULL;
+	
+	nodo->elemento = elemento;
+	nodo->siguiente = NULL;
+
+	return nodo;
 }
 
 lista_t *lista_insertar(lista_t *lista, void *elemento)
 {
+	if(lista == NULL)
+		return NULL;
+	nodo_t* nodo = nuevo_nodo(elemento);
+
+	if (nodo == NULL)
+		return NULL;
+
+	if(lista->espacios == 0){
+		lista->nodo_inicio = nodo;
+		lista->nodo_final = nodo;
+		lista->espacios = 1;
+		return lista;
+	}
+
+	lista->nodo_final->siguiente = nodo;
+	lista->nodo_final = nodo;
+	(lista->espacios)+=1;
+
+	return lista;
+}
+
+nodo_t* lista_nodo_en_posicion(lista_t* lista, size_t posicion){
+	if (lista == NULL || lista->espacios == 0)
+		return NULL;
+	
+	nodo_t* nodo = lista->nodo_inicio;
+	int i = 0;
+
+	while (i < posicion && i < (lista->espacios) && nodo->siguiente){
+		nodo = nodo->siguiente;
+		i++;
+	}
+
+	if (i == posicion)
+		return nodo;
+	
 	return NULL;
 }
 
 lista_t *lista_insertar_en_posicion(lista_t *lista, void *elemento,
 				    size_t posicion)
 {
-	return NULL;
+	if (lista == NULL)
+		return NULL;
+	
+	if (lista->espacios == 0 || posicion >= (lista->espacios))
+		return lista_insertar(lista, elemento);
+	
+	nodo_t* nodo = nuevo_nodo(elemento);
+
+	if (nodo == NULL)
+		return NULL;
+	
+	if (posicion == 0){
+		nodo->siguiente = lista->nodo_inicio;
+		lista->nodo_inicio = nodo;
+	} else {
+		nodo_t* nodo_anterior;
+		nodo_anterior = lista_nodo_en_posicion(lista, posicion - 1);
+		nodo->siguiente = nodo_anterior->siguiente;
+		nodo_anterior->siguiente = nodo;
+	}
+	
+	(lista->espacios)++;
+
+	return lista;
 }
 
 void *lista_quitar(lista_t *lista)
@@ -66,7 +147,7 @@ void *lista_ultimo(lista_t *lista)
 
 bool lista_vacia(lista_t *lista)
 {
-	return true;
+	return lista == false || lista->espacios == 0;
 }
 
 size_t lista_tamanio(lista_t *lista)
@@ -74,8 +155,19 @@ size_t lista_tamanio(lista_t *lista)
 	return 0;
 }
 
+void lista_destruir_nodos(nodo_t* nodo){
+	if (nodo){
+		lista_destruir_nodos(nodo->siguiente);
+		free(nodo);
+	}
+}
+
 void lista_destruir(lista_t *lista)
 {
+	if(lista){
+		lista_destruir_nodos(lista->nodo_inicio);
+		free(lista);
+	}
 }
 
 void lista_destruir_todo(lista_t *lista, void (*funcion)(void *))
